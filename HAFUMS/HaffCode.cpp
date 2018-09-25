be1b62bea2b07b8d103c9d3543e553e53bc076a9
寒泉 <1051576073@qq.com>
2018-09-25 17:22:09 +0800

    0925日常备份

--- HAFUMS/HaffCode.cpp
+++ HAFUMS/HaffCode.cpp
@@ -6,27 +6,93 @@ HaffCode::HaffCode()
 {
 }
 
-bool HaffCode::initTree(const char path[256])
+bool HaffCode::initTree(const char path[128])
 {
-		int totle[128];
-		memset(totle, 0, sizeof(totle));
-		IN in(path);
-		element c;
-		while (in(c))
-		{
-			if (c <= 0 || c > 128)
-				cout << "´íÎóµÄÊýÖµ" << int(c) << endl;
-			totle[c]++;
-
-		}
-		for (int i = 0; i < 128; i++)
-		{
-			if (totle[i] > 0)
-				cout << i << ":" << totle[i] << endl;
-		}
-		return 1;
-
+	codeChar(path);
+	cout << "count over!" << endl;
+	structTree();
+	cout << "tree over!" << endl;
+	initCodeMap(tree.size()-1);
+	cout << "map over!" << endl;
+	return true;
 }
+
+bool HaffCode::codeChar(const char path[128])
+{
+	int totle[256];
+	memset(totle, 0, sizeof(totle));
+	IN in(path);
+	element c;
+	while (in(c))
+	{
+		totle[c+OFFSET]++;
+	}
+	for (int i = 0; i < 256; i++)
+	{
+		if (totle[i] > 0)
+		{
+			code[i - OFFSET] = totle[i];
+			tree.push_back(HaffNode((char)(i - OFFSET), totle[i]));
+		}
+	}
+	return 1;
+}
+
+bool HaffCode::structTree()
+{
+	while (true)
+	{
+		int m1=-1, m2=-1;
+		int size = tree.size();
+		for (int i = 0; i < size; i++)
+		{
+			if (tree[i].parent == -1)
+			{
+				if (m1 == -1)
+				{
+					m1 = i;
+				}
+				else if(m2==-1)
+				{
+					if (tree[m1].weight < tree[i].weight)
+						m2 = i;
+					else
+					{
+						m2 = m1;
+						m1 = i;
+					}
+				}
+				else
+				{
+					if (tree[i].weight < tree[m1].weight)
+						m1 = i;
+					else if (tree[i].weight < tree[m2].weight)
+						m2 = i;
+
+				}
+			}
+		}
+		cout << "m1:" << m1 << ",m2:" << m2 << ",size:" << size << endl;
+		if (m2 == -1) break;
+		tree[m1].parent= tree[m2].parent = tree.size();
+		
+		tree.push_back(HaffNode(-1, tree[m1].weight + tree[m2].weight, -1, m1, m2));
+	}
+	return true;
+}
+
+bool HaffCode::initCodeMap(int pos,string str)
+{
+	if (tree[pos].rc == -1)
+	{
+		code[tree[pos].val] = str;
+		return true;
+	}
+	initCodeMap(tree[pos].lc, str + '0');
+	initCodeMap(tree[pos].lc, str + '1');
+	return true;
+}
+
 
 
 HaffCode::~HaffCode()
--- HAFUMS/HaffCode.h
+++ HAFUMS/HaffCode.h
@@ -2,6 +2,14 @@
 #ifndef HAFFCODE_H
 #define HAFFCODE_H
 #include "pch.h"
+#include <vector>
+#include <map>
+using namespace std;
+struct Header
+{
+	char orgin[128];
+	int size;
+};
 struct HaffNode
 {
 	char val;
@@ -9,12 +17,22 @@ struct HaffNode
 	int rc;
 	int lc;
 	int weight;
+
+	HaffNode(char _val, int _weight,int _parent=-1,int _rc=-1,int _lc=-1):val(_val),parent(_parent),rc(_rc),lc(_lc),weight(_weight)
+	{}
 };
+//×ó0ÓÒ1
 class HaffCode
 {
+	vector<HaffNode> tree;
+	map<char, string> code;
 public:
 	HaffCode();
-	bool initTree(const char path[256]);
+	//¹¹½¨¹ş·òÂüÊ÷£¬Óë±àÂëÓ³Éä
+	bool initTree(const char path[128]);
+	bool codeChar(const char path[128]);
+	bool structTree();
+	bool initCodeMap(int pos,string str="");
 	~HaffCode();
 };
 
